d3c522d1d35a8d27104297641f1ea51c9e01895c
Zaman <zamandiue44@gmail.com>
2016-10-13 01:20:08 +0600

    Update in 13/10/2016

--- 02. JavaScript Fundamental Course/05/js/basics.js
+++ 02. JavaScript Fundamental Course/05/js/basics.js
@@ -1,25 +1,16 @@
 //This is signle line comment
 /*This is multi
 line Comment
-var myVarName=20, secondVarName='Syed Zaman', another,lastone;
-
-	console.log(myVarName);
-	console.log(secondVarName);
-	console.log(another);
-	another=myVarName;
-	console.log(another);
 */
-
-
 console.log('This is just console log test');
 
-
-
 var number=1;
-var string='Hi, There';
-
-alert(quote);
-function a()
-{
-	var quote='"Hello", How are You?';
+var string='Hi, there';
+var quote='"Hello", \'How\' are you?';
+var array=[5,'hi, there?', true];//array[0]
+var object= {
+	myNumber: 10;
+	myString: "Syed Zaman";
 }
+object.myNumber;//10
+object.['myString'];//"Syed Zaman"
---
+++ 02. JavaScript Fundamental Course/08/js/basics.js
@@ -0,0 +1,12 @@
+//This is signle line comment
+/*This is multi
+line Comment
+
+	
+
+*/
+
+var object1= {
+	property1: true,
+	property2: 'Hello',
+};
