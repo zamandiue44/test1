13be052bd532e434898e85e9b289306449aee468
Zaman <zamandiue44@gmail.com>
2016-10-14 00:15:13 +0600

    Update 14.10.2016

--- 02. JavaScript Fundamental Course/08/js/basics.js
+++ 02. JavaScript Fundamental Course/08/js/basics.js
@@ -10,3 +10,16 @@ var object1= {
 	property1: true,
 	property2: 'Hello',
 };
+
+var one ='Hi, there';
+var oneObject=new String('Hi, There');
+
+
+function getColor(color){
+	var Things = {
+		blue: 'A Blue Color',
+		green: 'A Green Color',
+	}	
+};
+	return Things[color] || "Not Found";
+
---
+++ 02. JavaScript Fundamental Course/09/js/basics.js
@@ -0,0 +1,8 @@
+//This is signle line comment
+/*This is multi
+line Comment
+*/
+
+function myFunc(m){
+	console.log(m);
+}
